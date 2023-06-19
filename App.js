3e4f571e662ff0bfc6352f8fc7beaa0af2539433
meet <meet.mehta@senkusha.com>
2023-06-19 15:51:19 +0530

    initial learning'

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
-import React from "react";
-import { text, view } from "react-native";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, View, Text, Button } from "react-native";
 
 const App = () => {
+  const [count, setcount] = useState(1);
+
+  useEffect(() => {
+    console.warn("test use effect", count);
+  });
   return (
-    <view>
-      <text>API call</text>
-    </view>
+    <View style={styles.container}>
+      <Text style={styles.centeredText}>{count}</Text>
+      <Button title="update state" onPress={() => setcount(count + 1)} />
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  centeredText: {
+    textAlign: "center",
+    textAlignVertical: "center",
+  },
+});
+
+export default App;
